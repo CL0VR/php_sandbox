d0ff5f47d7ae46d04d84835d6fbce9b612a5be97
Zachary Dobbs <Dobbs@Zacharys-MacBook-Pro.local>
2016-02-12 13:39:44 -0700

    php_tut

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo "Oats" ?></title>
+    <link rel="stylesheet" href="./dist/css/styles.css">
+  </head>
+  <body>
+    <div class="main">
+      <header>
+        <a href="/"><?php echo "Oats" ?></a>
+        <nav>
+          <a href="/">Home</a>
+          <a href="#tourism">Tours</a>
+          <a href="#aboutism">About</a>
+          <a href="#contactism">Contact</a>
+        </nav>
+      </header>
+
+      <div class="oats">
+        <h3>(Overland. Adventure. Tours)</h3>
+        <p>
+          Founded in 1938 by grandpappy Foster, Oats was the brain-child of too much whiskey paired with a dash of "getting lost in the friggin woods". We here at Oats are happy to offer the same skills in survival and overland driving that got good ol' GP Foster on home.
+        </p>
+      </div>
+
+      <section class="tours">
+        <h2 id="tourism">Tours</h2>
+        <section class="row1">
+          <div class="family"></div>
+          <p>Family Pack</p>
+        </section>
+        <section class="row2">
+          <div class="extend"></div>
+          <p>Extended Pack</p>
+        </section>
+        <section class="row3">
+          <div class="fun"></div>
+          <p>Fun Pack</p>
+        </section>
+    </section>
+
+      <section class="about">
+        <h2 id="aboutism">ABOUT</h2>
+        <div class="worker1">
+          <div class="circle1"></div>
+          <h3>John Patricks</h3>
+          <p>
+            John Patricks, or JP as we like to call him is the most experienced of our staff, he's a bit of a stiff at times but he more than makes up for it with his incredible feats. At the age of 15 he climbed Mt. Everest in under a week. He fought a polar bear naked while vacationing in Serbia, and most recently swam from New York to Spain (any insinuations that Mr. Patricks is dead are incredulous and laughable)
+          </p>
+        </div>
+        <div class="worker2">
+          <div class="circle2"></div>
+          <h3>Pat Robinson</h3>
+          <p>
+            Pat Robinson was one of the lesser known children of the now infamous Swiss Family Robinson clan. Although never mentioned in the families countless journals she swears to God she did most of the work and single handedly saved her entire ungrateful family. Most days she can be found drinking the other employees under the table shouting, "BACK IN MY DAY..."
+          </p>
+        </div>
+        <div class="worker3">
+          <div class="circle3"></div>
+          <h3>Reggy "ACE" Osborn</h3>
+          <p>
+            Ace is by far our most enthusiastic team member and our very best driving coach! Always happy to help and with over 70yrs experience in the field of nature survival he was quite a catch. Great with kids and generally found playing hide-n-seek with JP, Ace is always ready to go whether it be four wheeling or digging to China.
+          </p>
+        </div>
+      </section>
+
+      <section class="contact">
+        <h2 id="contactism">CONTACT</h2>
+        <div class="social_links">
+        <div class="map"></div>
+        <div class="email">
+          <p>EMAIL US</p>
+        </div>
+      </section>
+
+      <footer><p>&copy; 2016 Oats LLC. All Rights Reserved. Designed by your Mother</p></footer>
+    </div>
+  </body>
+</html>
